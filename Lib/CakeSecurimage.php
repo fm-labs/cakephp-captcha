47cda540f9a4d9ef9c7cf32ea202f72a0e1c6fe4
fm-labs <flowmotionlabs@gmail.com>
2014-03-19 13:16:46 +0100

    code cleanup

--- Lib/CakeSecurimage.php
+++ Lib/CakeSecurimage.php
@@ -1,42 +1,45 @@
 <?php
-App::import('Vendor','Captcha.securimage'.DS.'securimage');
+App::import('Vendor', 'Captcha.securimage' . DS . 'securimage');
 
 class CakeSecurimage extends Securimage {
-	
+
+	public function __construct($options = array()) {
+		parent::__construct($options);
+	}
+
 	protected function checkTablesExist() {
 		//TODO do this the cakephp way
 		return parent::checkTablesExist();
 	}
-	
+
 	protected function clearCodeFromDatabase() {
 		//TODO do this the cakephp way
 		return parent::clearCodeFromDatabase();
 	}
-	
+
 	protected function createDatabaseTables() {
 		//TODO do this the cakephp way
 		return parent::createDatabaseTables();
 	}
-	
+
 	protected function purgeOldCodesFromDatabase() {
 		//TODO do this the cakephp way
 		return parent::purgeOldCodesFromDatabase();
 	}
-	
+
 	protected function openDatabase() {
 		//TODO do this the cakephp way
 		return parent::openDatabase();
 	}
-	
+
 	protected function saveCodeToDatabase() {
 		//TODO do this the cakephp way
 		return parent::saveCodeToDatabase();
 	}
-	
+
 	protected function getCodeFromDatabase() {
 		//TODO do this the cakephp way
 		return parent::getCodeFromDatabase();
 	}
-	
+
 }
-?>
